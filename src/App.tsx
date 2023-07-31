5cf879cd77040d54706f3f5c23c312dfaa7658b9
albertnt <alberto.tonet@gmail.com>
2023-07-31 12:25:09 +0200

    Codice pulito

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
 import Greeter from "./components/Greeter";
 import {
-  InputUsername2,
   InputUsername4,
-  UsernameModel,
   UsernameType
 } from "./components/InputUsername";
-import { Todolist, TodolistItem } from "./components/Todolist";
-import { CharCounter } from "./components/CharCounter";
 import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  //const [todos, setTodos] = useState<TodolistItem[]>([]);
 
   const [username, setUsername] = useState<UsernameType>({
     name: "User",
@@ -34,26 +29,13 @@ function App() {
     });
   };
 
-  //call first time only at component mount
-  /*useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
-      .then((res) => res.json())
-      .then((res) => setTodos(res));
-    //console.log(JSON.stringify(todos, null, "\t"));
-  }, []);
-  */
-
   return (
     <div className="App">
-      <Greeter value={usernameObj.name} fontSize="5vw" />
-      <CharCounter value={usernameObj.name} />
-      <br />
-      <br />
       <InputUsername4
         {...usernameObj}
         onUsernameObjChange={setUsernameObjName}
       />
-      <br />
+      <Greeter value={usernameObj.name} fontSize="5vw" />
     </div>
   );
 }
--- src/components/InputUsername.tsx
+++ src/components/InputUsername.tsx
@@ -1,81 +1,5 @@
 import React from "react";
-
-//modo vecchio di scrivere le funzioni
-/*export function InputUsername({ value }: { value: string }) {
-  console.log(value);
-  return (
-    <input
-      type="text"
-      id="username"
-      name="username"
-      onChange={handleUsernameChange}
-      value={value}
-    />
-  );
-}*/
-
-//modo nuovo e con return complessi e espliciti
-/*export const InputUsername = ({ value }: { value: string })  => {
-  console.log(value);
-  <input 
-    type="text" 
-    id="username" 
-    name="username" 
-    onChange={handleUsernameChange}
-    value={value}
-  />
-};*/
-
-// modo alternativo per funzioni atomiche "monoriga" e con return implicito
-/*export const InputUsername3 = ({ value }: { value: string }) => (
-  <input type="text" id="username" name="username" onChange={handleUsernameChange} value={value}/>
-);*/
-
-//React.FC serve per "ereditare" velocemente tutte le proprietà di react
-//non va usato sempre
-//originale non toccare
-/*export const InputUsername2: React.FC<UsernameModel> = ({ username }) => {
-  return (
-    <input
-      type="text"
-      id="username"
-      name="username"
-      onChange={handleUsernameChange}
-      value={username}
-    />
-  );
-};*/
-
-/*const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  console.log(event.target.value);
-}*/
-
-export interface UsernameModel {
-  name: string;
-  maxlength: number;
-  onUsernameChange?: (newType: string) => void;
-}
-
-export const InputUsername2 = ({ name, onUsernameChange }: UsernameModel) => {
-  return (
-    <>
-      <span>Type2: </span>
-      <input
-        type="text"
-        id="username"
-        name="username"
-        value={name}
-        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-          if (event.target.value.length <= 50) {
-            if (onUsernameChange) {
-              onUsernameChange(event.target.value);
-            }
-          }
-        }}
-      />
-    </>
-  );
-};
+import { CharCounter } from "../components/CharCounter";
 
 export interface UsernameType {
   name: string;
@@ -84,7 +8,6 @@ export interface UsernameType {
 }
 
 export const InputUsername4 = (props: UsernameType) => {
-  //console.log(props.name, props.maxlength);
   return (
     <>
       <span>Type4: </span>
@@ -97,11 +20,13 @@ export const InputUsername4 = (props: UsernameType) => {
           if (event.target.value.length <= props.maxlength) {
             if (props.onUsernameObjChange) {
               props.onUsernameObjChange(event.target.value);
-              //console.log(event.target.value);
             }
           }
         }}
       />
+      <br/>
+      <br/>
+      <CharCounter value={props.name} />
     </>
   );
 };
