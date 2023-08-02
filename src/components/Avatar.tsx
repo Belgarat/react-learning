a541a251fd08d08565cdcf83e77f61920aea94cc
albertnt <alberto.tonet@gmail.com>
2023-08-02 14:46:34 +0200

    Attached onClick to avatar icon

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,10 +1,14 @@
 import img_avatar from "./img_avatar.png"
 import "./Avatar.css"
 
+const handleClick = () => {
+  console.log("Clicked on avatar icon");
+}
+
 export const Avatar = () => {
   return(
     <>
-    <img src={img_avatar} alt="Avatar" className="avatar"/>
+    <img src={img_avatar} alt="Avatar" className="avatar" onClick={handleClick}/>
     </>
   )
 }
