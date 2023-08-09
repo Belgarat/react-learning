1b6fcef9c0482257629578dbb38efb5d4c89f664
albertnt <alberto.tonet@gmail.com>
2023-08-09 09:06:53 +0200

    button styling

--- src/components/ReplyCancelButton.tsx
+++ src/components/ReplyCancelButton.tsx
@@ -1,9 +1,12 @@
 import { CommentModel } from "../Interfaces";
+import "./ReplyForm.css";
 
 export const ReplyCancelButton = (props: CommentModel) => {
   return(
     <>
-      <button onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+      <button 
+      className="replyformflex-button-cancel"
+      onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
         if(props.clickCancel){
           props.clickCancel(event);
         }
--- src/components/ReplySaveButton.tsx
+++ src/components/ReplySaveButton.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import { CommentModel } from "../Interfaces"
+import "./ReplyForm.css";
 
 export const ReplySaveButton = (props: CommentModel) => {
   return(
     <>
       <button 
+      className="replyformflex-button-add"
       onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
           if(props.clickAdd && props.text.length>0){
             props.clickAdd(event);
