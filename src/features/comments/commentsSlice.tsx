9cb0c521cb7c8b07a9eb5d3fafce22c535235305
zcode <viacart@gmail.com>
2023-11-10 17:18:21 +0100

    Add interface to InitialState

--- src/features/comments/commentsSlice.tsx
+++ src/features/comments/commentsSlice.tsx
@@ -1,56 +1,11 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { CommentModel } from "../../Interfaces";
 
-const initialState = {
+const initialState: {me: number, status: string, error: string|null, comments: CommentModel[]} = {
   me: 1,
   status: "idle",
   error: null,
-  comments: [
-    /*{
-      id: 1,
-      attributes: {
-        body: "prova111",
-        likes: 3,
-        createdAt: "2023-09-18T08:44:57.064Z",
-        updatedAt: "2023-10-17T06:27:36.114Z",
-        publishedAt: "2023-09-18T09:12:07.561Z",
-        author: {
-          data: {
-            id: 1,
-            attributes: {
-              name: "John Smith",
-              email: "js@js.test",
-              createdAt: "2023-09-18T12:13:10.947Z",
-              updatedAt: "2023-09-18T12:13:11.934Z",
-              publishedAt: "2023-09-18T12:13:11.932Z",
-            },
-          },
-        },
-      },
-    },
-    {
-      id: 2,
-      attributes: {
-        body: "This is the sec6onfd commrent.\nBye",
-        likes: 5,
-        createdAt: "2023-09-18T10:07:12.949Z",
-        updatedAt: "2023-10-17T08:23:13.725Z",
-        publishedAt: "2023-09-18T10:07:31.007Z",
-        author: {
-          data: {
-            id: 2,
-            attributes: {
-              name: "Mary Jane",
-              email: "js22@jswqeqw.tst",
-              createdAt: "2023-09-25T12:22:43.761Z",
-              updatedAt: "2023-09-25T12:22:45.651Z",
-              publishedAt: "2023-09-25T12:22:45.648Z",
-            },
-          },
-        },
-      },
-    },*/
-  ],
+  comments: [],
 };
 
 export const commentsSlice = createSlice({
@@ -59,8 +14,7 @@ export const commentsSlice = createSlice({
   reducers: {
     commentAdded(state, action) {
       //console.log("commentAdded: ", state, action.payload);
-      const toAdd: any = action.payload;
-      state.comments.push(toAdd);
+      state.comments = [...state.comments, action.payload];
     },
     commentDeleted(state, action) {
       //console.log("commentDeleted: ", state, action.payload);
@@ -118,3 +72,47 @@ export const fetchPosts = createAsyncThunk(
     return response.data;
   }
 );
+ /*{
+      id: 1,
+      attributes: {
+        body: "prova111",
+        likes: 3,
+        createdAt: "2023-09-18T08:44:57.064Z",
+        updatedAt: "2023-10-17T06:27:36.114Z",
+        publishedAt: "2023-09-18T09:12:07.561Z",
+        author: {
+          data: {
+            id: 1,
+            attributes: {
+              name: "John Smith",
+              email: "js@js.test",
+              createdAt: "2023-09-18T12:13:10.947Z",
+              updatedAt: "2023-09-18T12:13:11.934Z",
+              publishedAt: "2023-09-18T12:13:11.932Z",
+            },
+          },
+        },
+      },
+    },
+    {
+      id: 2,
+      attributes: {
+        body: "This is the sec6onfd commrent.\nBye",
+        likes: 5,
+        createdAt: "2023-09-18T10:07:12.949Z",
+        updatedAt: "2023-10-17T08:23:13.725Z",
+        publishedAt: "2023-09-18T10:07:31.007Z",
+        author: {
+          data: {
+            id: 2,
+            attributes: {
+              name: "Mary Jane",
+              email: "js22@jswqeqw.tst",
+              createdAt: "2023-09-25T12:22:43.761Z",
+              updatedAt: "2023-09-25T12:22:45.651Z",
+              publishedAt: "2023-09-25T12:22:45.648Z",
+            },
+          },
+        },
+      },
+    },*/
